12cf16599004e3a32110b073c830bd76571f77fa
Mukund Bhudia <mukund_bhudia@hotmail.com>
2014-11-28 20:08:12 +0000

    Added the ability to view user details when selecting a user

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,10 +1,11 @@
 //User list array to fill info box
-var userList = [];
+var userListData = [];
 
 //When DOM loads...
 $(document).ready(function(){
 	//...populate the user table
 	populateTable();
+	$('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 });
 
 function populateTable() {
@@ -12,6 +13,7 @@ function populateTable() {
 
 	//Make an AJAX call to obtain users as JSON
 	$.getJSON('/users/userlist', function(data){
+		userListData = data;
 		//For each user, create a HTML row and from the table body
 		$.each(data, function(){
 			tableContent += '<tr>';
@@ -24,4 +26,19 @@ function populateTable() {
 		//add the generated table body content to the table on the index view
 		$('#userList table tbody').html(tableContent);
 	});
+}
+
+function showUserInfo(event) {
+	//Prevent the link from firing
+	event.preventDefault();
+
+	var thisUserName = $(this).attr('rel');
+	var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+	var thisUserObject = userListData[arrayPosition];
+
+	//Populate user info area
+	$('#userInfoName').text(thisUserObject.fullname);
+	$('#userInfoAge').text(thisUserObject.age);
+	$('#userInfoGender').text(thisUserObject.gender);
+	$('#userInfoLocation').text(thisUserObject.location);
 }
